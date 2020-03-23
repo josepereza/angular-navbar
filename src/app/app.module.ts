80ba4a1cf3c69a41d888f4775ee5aa5c01767e7f
josepereza <josepereza66@gmail.com>
2020-03-23 19:47:22 +0100

    segundo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule,
+  MatTableModule, MatPaginatorModule, MatCardModule, MatSelectModule, MatSortModule
+} from '@angular/material';
+import { HomeComponent } from './home-component/home-component.component';
+import { SelectComponent } from './select/select.component';
+import { FormsModule } from '../../node_modules/@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import {MatListModule} from '@angular/material/list';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    SelectComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, FlexLayoutModule,
+    FormsModule,
+    MatButtonModule, MatCardModule, MatIconModule,
+    MatPaginatorModule, MatSelectModule, MatSidenavModule,
+    MatSortModule, MatToolbarModule, MatTableModule,MatListModule  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
